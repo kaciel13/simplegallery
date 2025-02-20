63592a0cdf0e5123643e3d3f6c5b3fe3d24e724a
kaciel13 <kaciel13@yahoo.com>
2025-02-20 10:49:18 +0600

    add design home page

--- src/Home.js
+++ src/Home.js
@@ -1,24 +1,28 @@
 import './style/Home.css'
+import { Link } from 'react-router-dom';
 
 function Home() {
     return (
      
       <div className="Home">
         <div className='head'>
-          <header className="Header">Simple Gallery</header>
+          <header className="Header"><Link to="/" className="linkStyle">Simple Gallery</Link></header>
         </div>
-        
         <div className="Buttons">
             <div className="Bgallery">
-
+                  
             </div>
+            <div className='Flex'> 
             <div className="Bauthors">
     
             </div>
             <div className="Bgenre">
               
             </div>
+            </div>
         </div>
+        <footer className='footer'><Link to="https://github.com/kaciel13" className="linkStyle">@kaciel13</Link></footer>
+        <footer className='year'>2025</footer>
       </div>
     );
   }
