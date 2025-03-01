63286e7b08e082b55194a4998b4b645c4cecacaa
kaciel13 <kaciel13@yahoo.com>
2025-03-01 10:31:28 +0600

    Add 7 authors image

--- src/Authors.js
+++ src/Authors.js
@@ -1,4 +1,4 @@
-import './style/Genres.css'
+import './style/Authors.css'
 import { Link } from 'react-router-dom';
 
 function Authors() {
@@ -8,14 +8,20 @@ function Authors() {
         <div className='head'>
           <header className="Header"><Link to="/" className="linkStyle">Simple Gallery</Link></header>
         </div>
-        <div className='genres-buttons'>
+        <div className='authors-buttons'>
           <div className='row'>
-            <div className='genres bg-scenery'><div className='shade_box'><p className='textbutton'>Пейзажи</p></div></div>
-            <div className='genres bg-portret' ><div className='shade_box'><p className='textbutton'>Портреты</p></div></div>
-            <div className='genres bg-still-life'><div className='shade_box'><p className='textbutton'>Натюрморты</p></div></div>
-            <div className='genres bg-history'><div className='shade_box'><p className='textbutton'>Историческая живопись</p></div></div>
-            <div className='genres bg-abstraction'><div className='shade_box'><p className='textbutton'>Абстракция</p></div></div>
-            <div className='genres bg-genre-paint'><div className='shade_box'><p className='textbutton'>Жанровая живопись</p></div></div>
+            <div className='authors bg-leonardodavinchi'><div className='shade_box'><p className='textbutton'>Леонардо да Винчи</p></div></div>
+            <div className='authors bg-vangog' ><div className='shade_box'><p className='textbutton'>Ван Гог</p></div></div>
+            <div className='authors bg-claudemonet'><div className='shade_box'><p className='textbutton'>Клод Моне</p></div></div>
+            <div className='authors bg-pablopicaso'><div className='shade_box'><p className='textbutton'>Пабло Пикассо</p></div></div>
+            <div className='authors bg-michelangelobuanorotti'><div className='shade_box'><p className='textbutton'>Микеланджело Буонарроти</p></div></div>
+            <div className='authors bg-rembrantrein'><div className='shade_box'><p className='textbutton'>Рембрандт ван Рейн</p></div></div>
+            <div className='authors bg-salvadordali'><div className='shade_box'><p className='textbutton'>Сальвадор Дали</p></div></div>
+            <div className='authors bg-portret' ><div className='shade_box'><p className='textbutton'>Густав Климт</p></div></div>
+            <div className='authors bg-still-life'><div className='shade_box'><p className='textbutton'>Эдвард Мунк</p></div></div>
+            <div className='authors bg-history'><div className='shade_box'><p className='textbutton'>Джексон Поллок</p></div></div>
+            <div className='authors bg-abstraction'><div className='shade_box'><p className='textbutton'>Фрида Кало</p></div></div>
+            <div className='authors bg-genre-paint'><div className='shade_box'><p className='textbutton'>Анри Матисс</p></div></div>
           </div>
           
         </div>
